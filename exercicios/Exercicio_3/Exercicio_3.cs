ce57f91997e641ed14e907e6f51cc1ba0c4c9384
jessica bohn <jessicabohn74@gmail.com>
2021-03-03 20:29:24 -0300

    Adicionado Exercicio_3

---
+++ exercicios/Exercicio_3/Exercicio_3.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Exercicio_3
+{
+    class Exercicio_3
+    {
+        static void Main(string[] args)
+        {
+            int variavel_inteira = 10;
+            float variavel_float;
+            // typecast implícito
+            variavel_float = variavel_inteira;
+            System.Console.WriteLine(variavel_float);
+            // type explicíto
+            variavel_float = 5.5f;
+            variavel_inteira = (int)variavel_float;
+            System.Console.WriteLine(variavel_inteira);
+            // classe convert
+            int var_inteira = 15;
+            float  var_float = System.Convert.ToInt32(var_inteira);
+            System.Console.WriteLine(var_float);
+
+            var_float = 23.15f;
+            var_inteira = System.Convert.ToInt32(var_float);
+            System.Console.WriteLine(var_inteira);
+
+            
+
+
+
+        }
+    }
+}
