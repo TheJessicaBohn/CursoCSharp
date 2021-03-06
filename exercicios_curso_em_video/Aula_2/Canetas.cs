fc426f6bf7500958b29ffff30a08e8d7b08ca29e
jessica bohn <jessicabohn74@gmail.com>
2021-03-05 21:43:05 -0300

    Adicionando novos exercicios

---
+++ exercicios_curso_em_video/Aula_2/Canetas.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Aula_2
+{
+    class Canetas
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
