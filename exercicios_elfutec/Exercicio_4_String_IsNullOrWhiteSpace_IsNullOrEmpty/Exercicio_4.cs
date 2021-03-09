b8391becb0bde35aade38215bc838368b055c2fa
jessica bohn <jessicabohn74@gmail.com>
2021-03-08 21:46:53 -0300

    Correção do nome das pastas

---
+++ exercicios_elfutec/Exercicio_4_String_IsNullOrWhiteSpace_IsNullOrEmpty/Exercicio_4.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Exercicio_4
+{
+    class Exercicio_4
+    {
+        static void Main(string[] args)
+        {   
+            //Utilizando metodos IsNullOrWhiteSpace e IsNullOrEmpty
+            string entrada;
+            Console.WriteLine("Escreva algo ou nada:");
+            entrada = Console.ReadLine();
+
+            if(String.IsNullOrWhiteSpace(entrada) ||  String.IsNullOrEmpty(entrada) ){
+                Console.WriteLine("Nulo ou espaços em branco ou vazio");
+            }
+            else
+            {
+                Console.WriteLine(entrada);
+            }
+
+            // imprimindo decimo caractere
+            for( int i = entrada.Length; i >=0 ; i++){
+                Console.WriteLine(entrada[9]);
+                break;
+            }
+            
+        }
+    }
+}
