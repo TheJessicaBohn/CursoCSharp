81f7a0833724ab2469ef952e15415e16d61ed6ab
jessica bohn <jessicabohn74@gmail.com>
2021-03-30 21:47:12 -0300

    Create Exercicio_7

---
+++ exercicios_elfutec/Exercicio_7_StringBuilder/Exercicio_7.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Exercicio_7
+{
+    class Exercicio_7
+    {
+        static void Main(string[] args)
+        {
+           // crie um objeto do tipo StringBuilder e com a entrada do usuario crie uma quebra de linha 5 vezes
+
+           	Stringbuilder sb = new Stringbuilder();
+            Console.WriteLine("Palavra1");
+
+            Console.WriteLine("Palavra2");
+            Console.WriteLine("Palavra3");
+            Console.WriteLine("Palavra4");
+            Console.WriteLine("Palavra5");
+
+            // Adicionar uma linha com o texto "fim de leitura"
+
+            // imprimir todo o conteudo
+
+            // limpar o conteúdo do objeto
+
+            // Adicionar uma linha com o texto "novo conteúdo", e imprimir de novo
+
+
+
+
+        }
+    }
+}
