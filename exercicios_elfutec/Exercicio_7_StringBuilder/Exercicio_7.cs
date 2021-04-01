02db6b7508e85b0388301d28af68a338102392d0
jessica bohn <jessicabohn74@gmail.com>
2021-03-31 22:17:39 -0300

    Update Exercicio_7.cs

--- exercicios_elfutec/Exercicio_7_StringBuilder/Exercicio_7.cs
+++ exercicios_elfutec/Exercicio_7_StringBuilder/Exercicio_7.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace Exercicio_7
 {
@@ -8,25 +9,36 @@ namespace Exercicio_7
         {
            // crie um objeto do tipo StringBuilder e com a entrada do usuario crie uma quebra de linha 5 vezes
 
-           	Stringbuilder sb = new Stringbuilder();
+           	StringBuilder sb = new StringBuilder();
             Console.WriteLine("Palavra1");
-
+            sb.Append(Console.ReadLine());
+            sb.AppendLine();
             Console.WriteLine("Palavra2");
+            sb.Append(Console.ReadLine());
+            sb.AppendLine();
             Console.WriteLine("Palavra3");
+            sb.Append(Console.ReadLine());
+            sb.AppendLine();
             Console.WriteLine("Palavra4");
+            sb.Append(Console.ReadLine());
+            sb.AppendLine();
             Console.WriteLine("Palavra5");
-
+            sb.Append(Console.ReadLine());
+            sb.AppendLine();
+                      
             // Adicionar uma linha com o texto "fim de leitura"
+            sb.Append("Fim de Leitura");
+
 
             // imprimir todo o conteudo
+             Console.WriteLine(sb.ToString());
 
             // limpar o conteúdo do objeto
+            sb.Clear();
 
             // Adicionar uma linha com o texto "novo conteúdo", e imprimir de novo
-
-
-
-
+            sb.Append("Novo Conteudo");
+            Console.WriteLine(sb.ToString());
         }
     }
 }
