ca3eca96b46d8c5568ed8cd229cc23f65087634c
jessica bohn <jessicabohn74@gmail.com>
2021-04-08 07:12:12 -0300

    Criado Exercicio_8

---
+++ exercicios_elfutec/Exercicio_8_var/Exercicio_8.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exercicio_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Tipos implicitos com var
+
+            //Tipo integer
+            var inteiro = 1;
+            Console.WriteLine(inteiro);
+
+            //Tipo string;
+            var texto = "tipo string";
+            Console.WriteLine(texto);
+
+            //coleção de string
+            var dados= new[] {nome, idade, cidade};
+            Console.WriteLine(colecaos);
+
+            //for
+
+            for (var indice = 0; indice <= 3; indice++)
+            Console.WriteLine(String.Format("Indice 'indice' :{0}", indice.GetType().ToString()));
+
+            //foreach
+            foreach(var dado in dados)
+            Console.WriteLine(String.Format(" Tipo de 'dado' :{0}", dado.GetType().ToString()));
+
+            //consulta de LINQ
+
+        }
+    }
+}
